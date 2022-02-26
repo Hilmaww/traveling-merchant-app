b26c7c6fe6fdd16dac3be785b693d87aadbe6117
AdityaP1502 <73848290+AdityaP1502@users.noreply.github.com>
2022-02-26 22:13:02 +0700

    Update App.tsx

--- merchant/App.tsx
+++ merchant/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./src/screens/Home"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
@@ -18,3 +18,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
