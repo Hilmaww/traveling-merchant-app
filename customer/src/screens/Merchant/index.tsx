74cab9c2ef8c2a78a7c5de0500144f5c5c9ac8a3
chandrew29 <98937153+chandrew29@users.noreply.github.com>
2022-02-27 02:45:35 +0700

    desc merchant tapi masi hrs revisi

--- customer/src/screens/Merchant/index.tsx
+++ customer/src/screens/Merchant/index.tsx
@@ -1,11 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Axios from 'axios'
+import {} from '../Home'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-    </View>
-  );
+
+  const[name, setName] = React.useState();
+  const[desc, setDesc] = React.useState();
+  const[phone, setPhone] = React.useState()
+  
+  React.useEffect(() => {
+    // Line 15, tinggal diganti dengan "../merchants/${id}"
+    Axios.get('https://travelling-merchant-app.herokuapp.com/v1/api/merchants/2')
+    .then((res:any) => {
+      
+      setName(res.data.name);
+      setPhone(res.data.phone);
+      setDesc(res.data.description)
+
+    })
+    
+  })
+  
+    return (
+      <View style={styles.container}>
+
+        <Text>{name}</Text>
+        <Text>{desc}</Text>
+        <Text>{phone}</Text>
+        <Text>{}</Text>
+
+      </View>
+    );
+  
 }
 
 const styles = StyleSheet.create({
